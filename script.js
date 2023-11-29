86bc37409030a1e6802cf7028efff4bd830ad988
Jellyel <jellyelkennesy@gmail.com>
2023-11-29 14:29:22 -0300

    ajutes nav_bar

---
+++ script.js
@@ -0,0 +1,15 @@
+let lista_nav = document.getElementById("lista_nav");
+let chek_btn = document.getElementById("check")
+
+function ocultar_nav(){
+    lista_nav.style.display = "none"
+}
+
+function teste_nav(){
+    if (lista_nav.style.display == "none"){
+        lista_nav.style.display = "block"
+    } else {
+        lista_nav.style.display == "none"
+    }
+    
+}
